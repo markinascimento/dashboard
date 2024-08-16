6e9d7ab5162ff316e5e64b5440216b80f5618214
marcos.santos <marcos.santos@armazempb.com.br>
2024-08-16 14:55:23 -0300

    style: adjuste in card for you last reviews

--- src/app/(dashboard)/components/LastReviewCard.tsx
+++ src/app/(dashboard)/components/LastReviewCard.tsx
@@ -6,12 +6,14 @@ interface ILastReviewCardProps {
 
 export function LastReviewCard({ note, name }: ILastReviewCardProps) {
   return (
-    <div className="flex items-center justify-evenly bg-royal-blue-100 p-2 rounded-2xl">
-      <div className="flex items-center justify-center w-9 h-9 bg-royal-blue-500 rounded-full">
-        <strong className="text-white"> {note} </strong>
-      </div>
+    <div className="flex items-center justify-between bg-royal-blue-100 p-2 rounded-2xl gap-4">
+      <div className="flex items-center gap-2">
+        <div className="flex items-center justify-center min-w-9 min-h-9 bg-royal-blue-500 rounded-full">
+          <strong className="text-white"> {note} </strong>
+        </div>
 
-      <strong> {name} </strong>
+        <strong className="text-sm"> {name} </strong>
+      </div>
 
       <button className="bg-royal-blue-500 px-4 py-1 rounded-full">
         <strong className="text-white text-sm tracking-[2px]">
